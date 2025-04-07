235553a1b767aacc2a5911f411c22e706bc8e10a
LPC249018 LPC249018 <LPC249018@G305-CC0243.local>
2025-04-07 12:43:56 +0800

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+import express, { json } from "express";
+
+const port = 8000;
+
+const app = express();
+
+app.use(json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/user', (req,res) => {
+    const {username, password } = req.body
+    if(username.length < 5){
+        res.status(400).send({
+            success: false,
+            message: "Username must be contain 5 or more letters!"
+        }).end();
+    }
+    if(password.length <= 8){
+        res.send({
+            success: false,
+            message: "Password must contain 8 or more characters"
+        }).end();
+    }
+    res.send("success: true")
+})
+
+app.listen(port, () =>{
+    console.log(`Server running at http://localhost:${port}/`)
+})
